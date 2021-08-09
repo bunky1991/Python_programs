809f910fb82d672e168126c57e1f0aab1908d24a
bunky1991 <samuelbancroft@outlook.com>
2021-08-09 23:04:27 +0100

    Create CSV_maker.py

---
+++ CSV_maker.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import csv
+
+def open_csv() -> None:
+    """
+    :return: None
+    """
+    doesFileExist = input("Does the file already exist: ").lower()
+    if doesFileExist == "no":
+        fileName = input("Enter new file name: ")
+        path = input("Enter file path in the following format: C:\\Desktop\\Folder\\CSVFiles\\")
+        try:
+            with open(path + fileName + ".csv", "w") as file:
+                writer = csv.writer(file)
+                addNext = True
+                wordList = []
+                while addNext:
+                    for i in range(5):
+                        word = input("Enter a word to add to list: ")
+                        wordList.append(word)
+                    next = input("Do you want to add another 5")
+                    if next == "Yes" or next == "yes":
+                        pass
+                    elif next == "No" or next == "no":
+                        addNext = False
+                writer.writerow(wordList)
+
+            with open(path + fileName, "w") as file:
+                print(file)
+
+        except FileExistsError:
+            print("File already exists")
+        except FileNotFoundError:
+            print(f"Couldnt find the file at {path}")
+        except:
+            print("Unknown error occured.")
+
+    if doesFileExist == "no":
+        fileName = input("Enter new file name: ")
+        path = input("Enter file path in the following format: C:\\Desktop\\Folder\\CSVFiles\\")
+        try:
+            with open(path + fileName + ".csv", "a") as file:
+                writer = csv.writer(file)
+                addNext = True
+                wordList = []
+                while addNext:
+                    for i in range(5):
+                        word = input("Enter a word to add to list: ")
+                        wordList.append(word)
+                    next = input("Do you want to add another 5")
+                    if next == "Yes" or next == "yes":
+                        pass
+                    elif next == "No" or next == "no":
+                        addNext = False
+                writer.writerow(wordList)
+
+            with open(path + fileName, "w") as file:
+                print(file)
+
+        except FileExistsError:
+            print("File already exists")
+        except FileNotFoundError:
+            print(f"Couldnt find the file at {path}")
+        except:
+            print("Unknown error occured.")
+
+
+
